44c482fa6707968ac3557db01258b3d1a90b0399
FilippiMnz <imperilkill12@gmail.com>
2025-06-09 11:03:16 -0300

    Remocao e Busca

--- main.c
+++ main.c
@@ -40,6 +40,49 @@ void InserirInicio(ListaC *lista, const char* nome, const char* cpf){
     }
 }
 
+void Sortear( ListaC *lista, int num){
+
+}
+
+void removerQualquer(ListaC *lista, int N){
+    if(lista->fim ==NULL || N < 0 || N >= lista->tam){
+        printf("\nVazio");
+        return;
+    }
+    No *remover = NULL;
+    if(lista->tam == 1){
+        remover = lista->fim;
+        lista->fim = NULL;
+    }else{
+        No *anterior;
+        if(N == 0){
+            anterior = lista->fim;
+        }else{
+            anterior = BuscarElementoIndice(lista, N - 1);
+        }
+        remover = anterior->proximo;
+        anterior->proximo = remover->proximo;
+        if(remover == lista->fim){
+            lista->fim = anterior;
+        }
+    }
+    printf("Nome: %s, CPF: %s\n", remover->nome, remover->CPF);
+    free(remover);
+    lista->tam--;
+}
+
+void BuscarElementoIndice(ListaC *lista, int N){
+    if(lista->fim ==NULL){
+        printf("\nVazio");
+        return;
+    }
+    No *aux = lista->fim->proximo;
+    for(int i = 0; i < N; i++){
+        aux = aux->proximo;
+    }
+    return aux;
+}
+
 void InserirFim(ListaC *lista, const char* nome, const char* cpf){
     No *aux, *novoNo = malloc(sizeof(No));
 
